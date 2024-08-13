87144f3bb34d63d10a00ed68388f86976b01b9c4
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-08-13 18:11:50 +0400

    Fixing Y.js shared object
    - Remove unused import 'CodeExamples' from 'CodeEditor.tsx'
    - Initialize 'yText' with a check for empty content before inserting
    file content

--- frontend/src/components/CodeEditor/CodeEditor.tsx
+++ frontend/src/components/CodeEditor/CodeEditor.tsx
@@ -6,7 +6,7 @@ import { CodemirrorBinding } from 'y-codemirror';
 import RandomColor from 'randomcolor';
 import { LanguageSelector, ThemeSelector } from './index';
 import { WebsocketProvider } from 'y-websocket';
-import { codeExamples, LanguageCode } from '../../utils/codeExamples';
+import { LanguageCode } from '../../utils/codeExamples';
 import { Editor } from 'codemirror';
 
 const languageModes: Record<LanguageCode, string> = {
@@ -37,22 +37,24 @@ const CodeEditor: React.FC<CodeEditorProps> = ({
     const ydoc = new Y.Doc();
     const provider = new WebsocketProvider(
       'ws://localhost:9090',
-      projectId, // projectId as room name
+      projectId,
       ydoc,
     );
 
     provider.on('status', (event: { status: string }) => {
-      console.log(event.status); // logs "connected" or "disconnected"
+      console.log(`WebSocket status: ${event.status}`);
     });
 
     const awareness = provider.awareness;
     const color = RandomColor();
     awareness.setLocalStateField('user', { name: 'User', color }); //TODO: Use a default until we implement the redux store
 
-    const yText = ydoc.getText(fileId); // fileId to create a Y.Text type
-    yText.insert(0, fileContent); // Initialize the editor with the file content
-    const yUndoManager = new Y.UndoManager(yText);
+    let yText = ydoc.getText(fileId);
+    if (yText.length === 0) {
+      yText.insert(0, fileContent);
+    }
 
+    const yUndoManager = new Y.UndoManager(yText);
     const binding = new CodemirrorBinding(yText, editorRef.current, awareness, {
       yUndoManager,
     });
