bbe2f3c15e5da5d4e2908d59bd561fa33a5882d6
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-08-21 09:51:05 +0400

    hotfix/ missing dependency

--- frontend/src/components/CodeEditor/index.ts
+++ frontend/src/components/CodeEditor/index.ts
@@ -1,3 +1,2 @@
 export { default as LanguageSelector } from '../Bars/LanguageSelector';
 export { default as ThemeSelector } from '../Bars/ThemeSelector';
-export { default as ConnectionForm } from './ConnectionForm';
