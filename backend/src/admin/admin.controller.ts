d6680bb45039dacbe5258b2af5f378bbf487e037
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-08-21 07:51:08 +0400

    Oh, finaly 🤕: Resulving conflicts

--- backend/src/admin/admin.controller.ts
+++ backend/src/admin/admin.controller.ts
@@ -13,7 +13,9 @@ import { Users } from '@users/user.entity';
 import { Roles } from '@auth/decorators/roles.decorator';
 import { RolesGuard } from '@auth/guards/roles.guard';
 import { Role } from '@auth/enums/role.enum';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Admin')
 @Controller('admin')
 export class AdminController {
   constructor(
@@ -21,6 +23,11 @@ export class AdminController {
     private readonly usersService: UsersService,
   ) {}
 
+  @ApiOperation({
+    summary: 'Retrieve All Users',
+    description:
+      'Retrieve a list of all users in the system. Accessible only by admins.',
+  })
   @Get('all')
   @UseGuards(RolesGuard)
   @Roles(Role.Admin)
@@ -28,6 +35,10 @@ export class AdminController {
     return this.usersService.findAll();
   }
 
+  @ApiOperation({
+    summary: 'Delete All Users',
+    description: 'Delete all users from the system. Accessible only by admins.',
+  })
   @Delete('all')
   @UseGuards(RolesGuard)
   @Roles(Role.Admin)
@@ -35,6 +46,11 @@ export class AdminController {
     return this.usersService.removeAll();
   }
   // TODO: regular user should not be able to set roles
+  @ApiOperation({
+    summary: 'Retrieve User Profile by Username',
+    description:
+      'Retrieve a specific user profile by username. Accessible only by admins.',
+  })
   @UseGuards(RolesGuard)
   @Roles(Role.Admin)
   @Get('profile/:username')
@@ -42,18 +58,32 @@ export class AdminController {
     return this.usersService.findOneBy({ username });
   }
 
+  @ApiOperation({
+    summary: 'Update User Profile by Username',
+    description:
+      'Update a specific user profile by username. Accessible only by admins.',
+  })
   @UseGuards(RolesGuard)
   @Roles(Role.Admin)
   @Patch('profile/:username')
-  async updateUserAdmin(@Param('username') username: string, @Body() updateUserDto: Partial<Users>): Promise<Users> {
+  async updateUserAdmin(
+    @Param('username') username: string,
+    @Body() updateUserDto: Partial<Users>,
+  ): Promise<Users> {
     return this.usersService.updateByUsername(username, updateUserDto);
   }
 
+  @ApiOperation({
+    summary: 'Delete User Profile by Username',
+    description:
+      'Delete a specific user profile by username. Accessible only by admins.',
+  })
   @UseGuards(RolesGuard)
   @Roles(Role.Admin)
   @Delete('profile/:username')
-  async removeUserAdmin(@Param('username') username: string): Promise<{ message: string }> {
-  return this.usersService.removeByUsername(username);
+  async removeUserAdmin(
+    @Param('username') username: string,
+  ): Promise<{ message: string }> {
+    return this.usersService.removeByUsername(username);
   }
-
 }
