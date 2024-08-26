28b03995a047eb544a41315e7fc08876b9583e5e
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-08-26 13:10:38 +0300

    Add new origin to cors

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -14,6 +14,7 @@ export const corsConfig = {
       'http://localhost:1234',
       'https://collabor8-socket.abdallah.tech',
       'https://co11abor8.netlify.app/',
+      'https://co11abor8.netlify.app',
     ];
 
     if (!origin || allowedOrigins.includes(origin)) {
