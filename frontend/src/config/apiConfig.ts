3a8232e783b613f2e0a82fd3d0107c1eee536e14
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-08-26 12:07:18 +0300

    Update api config file in frontend

--- frontend/src/config/apiConfig.ts
+++ frontend/src/config/apiConfig.ts
@@ -1,7 +1,7 @@
 const env: { [key: string]: string } = {
   development: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api/v1',
   test: 'http://localhost:3000/api/v1',
-  production: 'https://www.collabor8.com',
+  production: 'https://co11abor8.netlify.app/',
 };
 
 const apiConfig = {
