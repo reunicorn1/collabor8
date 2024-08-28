555a14d87862d2ad577b2257d5ac73a7ea02e617
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-08-28 12:24:31 +0400

    Hot Fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,38 +14,26 @@ import {
 } from '@pages/index';
 import './App.css';
 import InviteGuest from '@components/Invite';
-import { AuthProvider } from './context/AuthContext';
-import ProtectedRoute from '@components/ProtectedRoute';
 
 const App: React.FC = () => {
   useAuthRefresh();
   useLogOut();
 
   return (
-    <AuthProvider>
-      {' '}
-      {/* Wrap the whole app with AuthProvider */}
+    <>
+      {/* <NavigationBar /> */}
       <Routes>
-        <Route path="/mission" element={<About />} />
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/dashboard"
-          element={<ProtectedRoute element={<Dashboard />} />}
-        />
-        <Route
-          path="/profile"
-          element={<ProtectedRoute element={<Profile />} />}
-        />
-        <Route
-          path="/editor/:projectId"
-          element={<ProtectedRoute element={<Editor />} />}
-        />
+        <Route path="/mission" element={<About />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/editor/:projectId" element={<Editor />}></Route>
         <Route path="/reset-password" element={<ResetPasswordModal />} />
         <Route path="/verify" element={<Verify />} />
         <Route path="/invite" element={<InviteGuest />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
-    </AuthProvider>
+    </>
   );
 };
 
