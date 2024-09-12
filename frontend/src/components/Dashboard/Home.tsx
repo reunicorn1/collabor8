2c0decc1737f2aae5a48910adf116602bcaab0f6
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-09-12 04:15:46 +0400

    Remove unnecessary flexDirection in Home component

--- frontend/src/components/Dashboard/Home.tsx
+++ frontend/src/components/Dashboard/Home.tsx
@@ -124,7 +124,7 @@ export default function Home() {
   // When this happens userDetails disappear, but accessToken is still there
 
   return (
-    <Flex justifyContent="center" h="100vh" flex={1} flexDirection="column">
+    <Flex justifyContent="center" h="100vh" flex={1}>
       <Box>
         <Image src="/banner3.png" maxW="100%" />
         <Center>
