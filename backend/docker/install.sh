f6ff751536c29328d40a57227e59a75454b95b6e
mo7amedElfadil <mohamedelfadil1994@gmail.com>
2024-09-16 18:54:28 +0300

    feat/install docker script

---
+++ backend/docker/install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+function installDocker(){
+	# Add Docker's official GPG key:
+	sudo apt-get update
+	sudo apt-get install ca-certificates curl
+	sudo install -m 0755 -d /etc/apt/keyrings
+	sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+	sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+	$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+	sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	sudo apt-get update
+	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+	sudo docker run hello-world
+}
+
+
+# check if docker is installed
+if ! [ -x "$(command -v docker)" ]; then
+  echo 'Error: docker is not installed.' >&2
+  installDocker
+else
+  echo 'docker is installed.'
+fi
+
+./javascript_container/install.sh
+./python_container/install.sh
+
+
