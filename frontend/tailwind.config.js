02c9ed56ac6d549346b7324c566737f2b0e2477b
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-09-22 03:01:25 +0400

    chore: remove deprecated purge option from tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,23 +1,5 @@
-/**
- * const colors = {
-  brand: {
-    900: '#001845',
-    800: '#524175',
-    700: '#333333',
-    600: '#E6E85C',
-    500: '#F3F3F3',
-    400: '#FFD700',
-    300: '#FF4C4C',
-    200: '#6BE3E1',
-    150: '#D1D1D1',
-    100: '#524175',
-  },
-};
-*/
-
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
